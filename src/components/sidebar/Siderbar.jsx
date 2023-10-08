d4db6ae54d327f29c93c52504279cd3a8e90b026
asder22767 <abrorbek22767@gmail.com>
2023-10-08 01:58:36 -0700

    developing sidebar

---
+++ src/components/sidebar/Siderbar.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef } from "react";
+import {
+  config,
+  creditcard,
+  home,
+  lineafterlogo,
+  logo,
+  tables,
+} from "../../assets/images";
+
+const Siderbar = () => {
+  const btnRef = useRef(null);
+  useEffect(() => {
+    const isActive = () => {
+      if (btnRef.current == document.activeElement) {
+        console.log("it is");
+      } else {
+        console.log("it is not");
+      }
+      document.addEventListener("focus", isActive);
+      return () => {
+        document.removeEventListener("focus", isActive);
+      };
+    };
+  });
+  const activeLink =
+    "flex items-center px-4 py-3 bg-sidebarActive rounded-[15px] cursor-pointer";
+  const normalLink =
+    "flex items-center px-4 py-3 rounded-[15px] cursor-pointer";
+  return (
+    <>
+      <div className="px-5 h-full py-5">
+        <div className="w-[264px] max-h-[890] h-full rounded-[20px] bg-[#001e53] ">
+          <div className="wrapper">
+            <div className="wrapper-top flex flex-col pt-7 mb-5">
+              <a href="#" className="self-center w-[135px] h-[14px] mb-7">
+                <img
+                  src={logo}
+                  alt="logo of warehouse"
+                  className=" w-[135px] h-[14px]"
+                />
+              </a>
+              <img src={lineafterlogo} alt="line" />
+            </div>
+            <div className="wrapper-bottom px-5">
+              <ul className="nav flex flex-col text-white gap-y-1 pb-3">
+                <li ref={btnRef} className={normalLink}>
+                  <div className="p-2 rounded-xl bg-iconBg-active active:bg-iconBg-active mr-3">
+                    <img src={home} alt="icon" className="w-[15px] h-[15px]" />
+                  </div>
+                  <p className="text-sm font-jakartaSans">Dashboard</p>
+                </li>
+                <li className="flex items-center px-4 py-3 rounded-[15px]">
+                  <div className="p-2 rounded-xl bg-iconBg active:bg-iconBg-active mr-3">
+                    <img
+                      src={tables}
+                      alt="icon"
+                      className="w-[15px] h-[15px]"
+                    />
+                  </div>
+                  <p className="text-sm font-jakartaSans">Tables</p>
+                </li>
+                <li className="flex items-center px-4 py-3 rounded-[15px]">
+                  <div className="p-2 rounded-xl bg-iconBg active:bg-iconBg-active mr-3">
+                    <img
+                      src={creditcard}
+                      alt="icon"
+                      className="w-[15px] h-[15px]"
+                    />
+                  </div>
+                  <p className="text-sm font-jakartaSans">Billing</p>
+                </li>
+                <li className="flex items-center px-4 py-3 rounded-[15px]">
+                  <div className="p-2 rounded-xl bg-iconBg active:bg-iconBg-active mr-3">
+                    <img
+                      src={config}
+                      alt="icon"
+                      className="w-[15px] h-[15px]"
+                    />
+                  </div>
+                  <p className="text-sm font-jakartaSans">RTL</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Siderbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
+
+    extend: {
+      colors: {
+        "dark-blue": "rgba(6, 11, 38, 0.94)",
+        brownish: "rgba(26, 31, 55, 0.00)",
+        iconBg: "#1A1F37",
+        "iconBg-active": "#0075FF",
+        sidebarActive: "#1A1F37",
+      },
+      fontFamily: {
+        jakartaSans: "Plus Jakarta Sans",
+      },
+    },
   },
   plugins: [],
-}
-
+};
